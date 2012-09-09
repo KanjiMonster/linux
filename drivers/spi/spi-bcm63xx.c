9270743739af0095093b976b60d9a4f747249381
Jonas Gorski <jonas.gorski@gmail.com>
2012-09-10 01:26:55 +0200

    SPI: spi-bcm63xx: use clk_{prepare_enable,disable_unprepare}
    Use proper clk_prepare/unprepare calls in preparation for switching
    to the generic clock framework.
    
    Signed-off-by: Jonas Gorski <jonas.gorski@gmail.com>

--- drivers/spi/spi-bcm63xx.c
+++ drivers/spi/spi-bcm63xx.c
@@ -423,7 +423,7 @@ static int __devinit bcm63xx_spi_probe(struct platform_device *pdev)
 	}
 
 	/* Initialize hardware */
-	clk_enable(bs->clk);
+	clk_prepare_enable(bs->clk);
 	bcm_spi_writeb(bs, SPI_INTR_CLEAR_ALL, SPI_INT_STATUS);
 
 	/* register and we are done */
@@ -439,7 +439,7 @@ static int __devinit bcm63xx_spi_probe(struct platform_device *pdev)
 	return 0;
 
 out_clk_disable:
-	clk_disable(clk);
+	clk_disable_unprepare(clk);
 out_err:
 	platform_set_drvdata(pdev, NULL);
 	spi_master_put(master);
@@ -460,7 +460,7 @@ static int __devexit bcm63xx_spi_remove(struct platform_device *pdev)
 	bcm_spi_writeb(bs, 0, SPI_INT_MASK);
 
 	/* HW shutdown */
-	clk_disable(bs->clk);
+	clk_disable_unprepare(bs->clk);
 	clk_put(bs->clk);
 
 	platform_set_drvdata(pdev, 0);
